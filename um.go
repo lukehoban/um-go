1afaccb34b641098f2d72ebb335242050cd96f06
Luke Hoban <lukehoban@gmail.com>
2015-07-30 21:36:46 +0530

    Almost working

--- um.go
+++ um.go
@@ -4,14 +4,13 @@ import (
     "io/ioutil"
     "fmt"
     "bytes"
-    "errors"
     "encoding/binary"
 )
 
 func run(program []uint32) {
     reg := [8]uint32{0,0,0,0,0,0,0,0}
     platters := [][]uint32{program}
-    pc := 0
+    var pc uint32 = 0
     
     for ;; {
         instruction := platters[0][pc]
@@ -19,14 +18,29 @@ func run(program []uint32) {
         a := ((instruction >> 6) & 7)
         b := ((instruction >> 3) & 7) 
         c := ((instruction >> 0) & 7)
-        fmt.Printf("PC: %d\n", pc)
-        fmt.Printf("Instruction: %0b\n", instruction)
-        fmt.Printf("Operation: %d\n", op)
+        // fmt.Printf("PC: %d\n", pc)
+        // fmt.Printf("Instruction: %032b\n", instruction)
+        // fmt.Printf("Operation: %d\n", op)
         switch op {
             case 0: if reg[c] != 0 { reg[a] = reg[b] }
-            default: panic(errors.New("Failed")) 
+            case 1: reg[a] = platters[reg[b]][reg[c]]
+            case 2: platters[reg[a]][reg[b]] = reg[c]
+            case 3: reg[a] = reg[b] + reg[c]
+            case 4: reg[a] = reg[b] * reg[c]
+            case 5: reg[a] = reg[b] / reg[c]
+            case 6: reg[a] = ^(reg[b] & reg[c])
+            case 7: return
+            case 8: { platters = append(platters, make([]uint32, reg[c])); reg[b] = uint32(len(platters) - 1) }
+            case 9: { platters[reg[c]] = nil }
+            case 12: {
+                if reg[b] != 0 { copy(platters[0], platters[reg[b]]) };
+                pc = reg[c];
+                continue
+            }
+            case 13: reg[(instruction >> 25) & 7] = instruction & 0x01FFFFFF
+            default: panic(fmt.Errorf("Failed on %d", op)) 
         }
-        fmt.Printf("%d\n", instruction)
+        pc++
     }
     
     fmt.Printf("%A\n", reg)
@@ -36,7 +50,7 @@ func read_platters(path string) []uint32 {
     b, err := ioutil.ReadFile(path)
     check(err)
     platters := make([]uint32, len(b) / 4)
-    err = binary.Read(bytes.NewReader(b), binary.LittleEndian, &platters)
+    err = binary.Read(bytes.NewReader(b), binary.BigEndian, &platters)
     check(err)
     return platters
 }
